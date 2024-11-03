5ed73df49e9fd65c8093b2cdb82c83ea339f49eb
KingSplatt <malp18774@gmail.com>
2024-11-03 15:30:46 -0800

    hola

---
+++ fetch_api4/js/personas.js
@@ -0,0 +1,25 @@
+const url = 'https://randomuser.me/api/';
+let boton = document.querySelector('button');
+let foto = document.querySelector('#foto');
+let nombre = document.querySelector('#nombre');
+let direccion = document.querySelector('#direccion');
+let telefono = document.querySelector('#telefono');
+
+const obtenerPersona = () => {
+    fetch(url)
+        .then(respuesta => respuesta.json()) //convierte la respuesta a json
+        .then(respuesta => {
+            //console.log(respuesta);
+            respuesta.results.forEach(persona => {
+                nombre.textContent = `${persona.name.first} ${persona.name.last}`;
+                foto.setAttribute('src', persona.picture.large);
+                direccion.textContent = `${persona.location.street.name} ${persona.location.street.number}`;
+                telefono.textContent = persona.phone;
+            });
+        })
+}
+
+boton.addEventListener('click', () => {
+    obtenerPersona();
+});
+obtenerPersona();
