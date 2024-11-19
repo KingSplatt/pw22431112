3ba0e3516c210c8098289926920a24c03488ecb1
KingSplatt <malp18774@gmail.com>
2024-11-19 16:25:43 -0700

    finalizado

--- vpractica_vue/cliente_tienda/src/modulos/principal/vistas/menuVue.vue
+++ vpractica_vue/cliente_tienda/src/modulos/principal/vistas/menuVue.vue
@@ -1,17 +1,30 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Tienda</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="#">Pricing</a>
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item margin">
+                        <RouterLink class="nav-link item" to="/clientes">Clientes</RouterLink>
+                    </li>
+                    <li class="nav-item margin">
+                        <RouterLink class="nav-link item" to="/personal">Personal</RouterLink>
+                    </li>
+                    <li class="nav-item margin">
+                        <RouterLink class="nav-link item" to="/articulos">Articulos</RouterLink>
+                    </li>
+                    <li class="nav-item margin">
+                        <RouterLink class="nav-link item" to="/ventas">Ventas</RouterLink>
+                    </li>
+                    <li class="nav-item margin">
+                        <RouterLink class="nav-link item" to="/registro">Registro</RouterLink>
+                    </li>
+                </ul>
+            </div>
             </div>
         </div>
     </nav>
--- vpractica_vue/cliente_tienda/src/router/index.ts
+++ vpractica_vue/cliente_tienda/src/router/index.ts
@@ -1,3 +1,4 @@
+import PersonalVue from '@/modulos/personal/vistas/PersonalVue.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -8,6 +9,11 @@ const router = createRouter({
     //   name: 'home',
     //   component: HomeView,
     // },
+    {
+      path: '/personal',
+      name: 'personal',
+      component: PersonalVue,
+    },
   ],
 })
 
